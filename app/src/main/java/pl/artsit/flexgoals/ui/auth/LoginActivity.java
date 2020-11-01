aa64b3495f6582f4abef46a1947ec7d11da5aa95
Kinga <kinga2233@onet.pl>
2020-11-01 21:10:17 +0100

    # FG-14 Wypenienie widoku Logowania elementami

--- app/src/main/java/pl/artsit/flexgoals/ui/auth/LoginActivity.java
+++ app/src/main/java/pl/artsit/flexgoals/ui/auth/LoginActivity.java
@@ -5,33 +5,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import pl.artsit.flexgoals.MainActivity;
 import pl.artsit.flexgoals.R;
+import pl.artsit.flexgoals.ui.home.HomeFragment;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private Button loginBtn;
+    private Button registrationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+         nameEditText = findViewById(R.id.nameEditText);
+         passwordEditText = findViewById(R.id.passwordEditText);
 
-        Button buttonRegister = findViewById(R.id.registerBtn);
-        buttonRegister.setOnClickListener(new View.OnClickListener(){
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                Intent intent = new Intent(getApplicationContext(), HomeFragment.class);
                 startActivity(intent);
             }
         });
-
-        Button buttonHome = findViewById(R.id.homeBtn);
-        buttonHome.setOnClickListener(new View.OnClickListener(){
+        registrationBtn = findViewById(R.id.registrationBtn);
+        registrationBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                MainActivity.isUser = true;
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
             }
         });
