c639fd97a9076000f44009929ab1b8ca8e8e4b76
Czakiss <zekse00@gmail.com>
2020-11-01 21:23:13 +0100

    connect GUI elements to RegisterActivity.java

--- app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
+++ app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
@@ -2,6 +2,8 @@ package pl.artsit.flexgoals.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import pl.artsit.flexgoals.R;
@@ -9,12 +11,20 @@ import pl.artsit.flexgoals.R;
 public class RegisterActivity extends AppCompatActivity {
     TextView textViewLogin;
     TextView textViewPassword;
+    EditText editTextLogin;
+    EditText editTextPassword;
+    EditText editTextPasswordRepeat;
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         textViewLogin= (TextView) findViewById(R.id.textViewLogin);
-
+        textViewPassword= (TextView) findViewById(R.id.textViewPassword);
+        editTextLogin= (EditText) findViewById(R.id.editTextLogin);
+        editTextPassword= (EditText) findViewById(R.id.editTextPassword);
+        editTextPasswordRepeat= (EditText) findViewById(R.id.editTextPasswordRepeat);
+        buttonRegister= (Button) findViewById(R.id.buttonRegister);
     }
 }
