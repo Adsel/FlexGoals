660edd0bfe918839c7d01fcf35c056211bea5e23
Czakiss <zekse00@gmail.com>
2020-11-01 21:17:01 +0100

    - corrected gradient.xml - added variable on strings.xml

--- app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
+++ app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
@@ -2,14 +2,19 @@ package pl.artsit.flexgoals.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import pl.artsit.flexgoals.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView textViewLogin;
+    TextView textViewPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        textViewLogin= (TextView) findViewById(R.id.textViewLogin);
+
     }
 }
