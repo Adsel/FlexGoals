42b466dd6b15815be5a540a3ae77d89e94018af3
Czakiss <zekse00@gmail.com>
2020-11-03 18:52:07 +0100

    correct design

--- app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
+++ app/src/main/java/pl/artsit/flexgoals/ui/auth/RegisterActivity.java
@@ -16,6 +16,7 @@ import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -95,6 +96,15 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            this.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         setContentView(R.layout.activity_register);
         textViewLogin= (TextView) findViewById(R.id.textViewLogin);
         textViewMail= (TextView) findViewById(R.id.textViewMail);
