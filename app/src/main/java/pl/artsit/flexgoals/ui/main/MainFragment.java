5f06eae38dd5fc6830a6300978dc840e56667f38
czakiss <zekse00@gmail.com>
2020-12-14 02:18:21 +0100

    correct design

--- app/src/main/java/pl/artsit/flexgoals/ui/main/MainFragment.java
+++ app/src/main/java/pl/artsit/flexgoals/ui/main/MainFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,13 +28,6 @@ public class MainFragment extends Fragment {
         mainViewModel =
                 new ViewModelProvider(this).get(MainViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        mainViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         HttpClient httpClient = new HttpClient(this);
         finalGoalRecyclerView = root.findViewById(R.id.final_goals_recycleview);
