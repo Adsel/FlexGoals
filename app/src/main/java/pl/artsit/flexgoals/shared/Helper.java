7ec7f978d8f1f8415800c2e82f96621e342d44dc
marcin radwan <marcin.radwan08@gmail.com>
2020-12-16 22:22:35 +0100

    Prepared to show left goals

--- app/src/main/java/pl/artsit/flexgoals/shared/Helper.java
+++ app/src/main/java/pl/artsit/flexgoals/shared/Helper.java
@@ -4,6 +4,10 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Helper {
+    public static final Integer GOAL_FINISHED = -1;
+    public static final Integer GOAL_ACHIVED = -1;
+
+
     public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
         long diffInMillies = date2.getTime() - date1.getTime();
         return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
