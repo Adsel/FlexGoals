904f92c06963578c5d885b07bdd4fa31f61e3961
czakiss <zekse00@gmail.com>
2021-01-06 02:36:30 +0100

    add translations

--- app/src/main/java/pl/artsit/flexgoals/MainActivity.java
+++ app/src/main/java/pl/artsit/flexgoals/MainActivity.java
@@ -1,11 +1,16 @@
 package pl.artsit.flexgoals;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -16,6 +21,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Locale;
 
 import pl.artsit.flexgoals.http.services.UserService;
 import pl.artsit.flexgoals.http.user.UserCallback;
@@ -39,10 +46,14 @@ public class MainActivity extends AppCompatActivity implements UserCallback {
     public static FinalGoalFlag previewFinalGoal;
     private DrawerLayout drawer;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO
+        setLocale(this,"EN");
 
         if(MainActivity.isUser) {
             Toolbar toolbar = findViewById(R.id.toolbar);
@@ -101,6 +112,16 @@ public class MainActivity extends AppCompatActivity implements UserCallback {
         } else {
             drawer.openDrawer(GravityCompat.START);
         }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    public static void setLocale(Activity activity, String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
     }
 }
 
